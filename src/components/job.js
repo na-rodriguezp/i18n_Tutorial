ac53de0f78e433ff2c2a242adb3a2af2e05f0a37
Nicolas Rodriguez <na.rodriguezp@uniandes.edu.co>
2023-05-16 09:28:50 -0500

    Tutorial

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,30 @@
 import React from "react";
-
+import {FormattedDate} from 'react-intl';
+import {FormattedPlural} from 'react-intl';
+import { FormattedNumber } from "react-intl";
 const Job = (props) => {
   return (
     <tr>
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+        {props.offer.salary}
+        <FormattedPlural value={props.offer.salary} one=" million" other=" millions" />
+      </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year='numeric'
+          month='long'
+          day='numeric'
+          weekday='long'
+        />
+      </td>
+      <td>
+        <FormattedNumber value={props.offer.views}/>
+      </td>
     </tr>
   );
 };
